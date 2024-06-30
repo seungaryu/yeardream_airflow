8368dbdf88a08fa58f65a606d9f08a6a8c0dfa0f
ryusa <yuseunga1995@gmail.com>
2024-07-01 00:40:55 +0900

    bulk load

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -4,19 +4,19 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 with DAG(
-        dag_id='dags_python_with_custom_hook_bulk_load',
+        dag_id='dags_python_with_postgres_hook_bulk_load',
         start_date=pendulum.datetime(2024, 6, 16, tz='Asia/Seoul'),
         schedule='0 7 * * *',
         catchup=False
 ) as dag:
     def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
-        custom_postgres_hook = CustomPostgresHook(postgres_conn_id=postgres_conn_id)
-        custom_postgres_hook.bulk_load(table_name=tbl_nm, file_name=file_nm, delimiter=',', is_header=True, is_replace=True)
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load(tbl_nm, file_nm)
 
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
         op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
-                   'tbl_nm': 'seoul_bike_hist',
-                   'file_nm': '/opt/airflow/ingest/bikeList/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/bikeList.csv'}
+                   'tbl_nm':'seoul_bike_hist',
+                   'file_nm':'/opt/airflow/ingest/bikeList/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/bikeList.csv'}
     )
